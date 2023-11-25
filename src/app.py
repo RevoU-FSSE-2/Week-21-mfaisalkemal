d7046295982d0b8cb655d5189a15f84e426b92bc
Muhammad Faisal Kemal <mfaisalkemal@gmail.com>
2023-11-25 11:59:35 +0700

    add all

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ from db import db, db_init
 from common.bcrypt import bcrypt
 from auth.apis import auth_blp
 from user.apis import user_blp
+from tweets.apis import tweet_blp
+from follows.apis import follow_blp
 
 app = Flask (__name__)
 
@@ -14,6 +16,9 @@ db.init_app(app)
 bcrypt.init_app(app)
 
 app.register_blueprint(auth_blp, url_prefix="/auth")
+app.register_blueprint(user_blp, url_prefix="/user")
+app.register_blueprint(tweet_blp, url_prefix="/tweet")
+app.register_blueprint(follow_blp, url_prefix="/following")
 
 
 # with app.app_context():
